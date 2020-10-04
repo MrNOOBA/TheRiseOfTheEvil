849230020b233f1641f91b4718d42ae9e96533ab
MrNOOBA <noah.wahlstrom@icloud.com>
2020-10-04 19:42:17 +0200

    Walk Animation

--- Assets/Characters/Jay/Movement.cs
+++ Assets/Characters/Jay/Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Security.Cryptography;
@@ -10,26 +11,43 @@ public class Movement : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpHeight = 5f;
     public bool isGrounded = false;
+    private Animator animator;
+    private Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
+        CheckSpeed();
+        var movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;        
     }
 
     void Jump()
     {
         if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+            rigidBody.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+
+    void CheckSpeed()
+    {
+        var speed = Math.Abs(Input.GetAxis("Horizontal"));        
+        if (speed > 0.01)
+        {
+            animator.SetFloat("Speed", 1);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
         }
     }
 
